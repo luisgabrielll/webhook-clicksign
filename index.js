e3bc6c4625242a33f959fce200cb7a870b565932
Luis <luis.andrade@markts.com.br>
2024-04-24 16:03:40 -0300

    addd cors

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+  console.log("TESTANDO LOG")
   res.send("Rota GET: Olá, mundo!");
 });
 
 app.post("/webhook", (req, res) => {
+  console.log('chamei')
   console.log(req.body);
-  res.send(`Rota POST: Mensagem recebida: ${mensagem}`);
 });
 
 app.listen(port, () => {
