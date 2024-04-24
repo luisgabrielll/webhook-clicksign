26f362b96d284cd204acf65b7b03d0fe23fbac07
Luis <luis.andrade@markts.com.br>
2024-04-24 14:46:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Rota GET: Olá, mundo!');
+});
+
+app.post('/webhook', (req, res) => {
+  const { mensagem } = req.body;
+  res.send(`Rota POST: Mensagem recebida: ${mensagem}`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
